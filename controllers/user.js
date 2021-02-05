dba4529d285fd89fb2050d7769ac36d67cf17a71
dhanush72 <dhanuraj7258@gmail.com>
2021-02-05 14:45:38 +0530

    added login, logout endpoint with jwt

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   try {
@@ -38,9 +39,77 @@ exports.register = async (req, res) => {
       password: hashPassword,
     }).save();
 
+    // * jwt token
+    const token = jwt.sign(
+      {
+        user: newUser._id,
+      },
+      process.env.SECRET
+    );
+
+    // * send token in cookie
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+      })
+      .send();
+
     res.json(newUser);
   } catch (error) {
     console.error("register error:", error);
     res.status(500).send();
   }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // * validation
+    if (!email || !password) {
+      return res.status(400).json({ error: "please enter all fileds" });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(401).json({ error: "wrong email or password" });
+    }
+
+    const currentPassword = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!currentPassword) {
+      return res.status(401).json({ error: "wrong email or password" });
+    }
+
+    // * jwt token
+    const token = jwt.sign(
+      {
+        user: existingUser._id,
+      },
+      process.env.SECRET
+    );
+
+    // * send token in cookie
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+      })
+      .send();
+  } catch (error) {
+    console.error("login error:", error);
+    res.status(500).send();
+  }
+};
+
+exports.logout = async (req, res) => {
+  res
+    .cookie("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    })
+    .send();
 };
